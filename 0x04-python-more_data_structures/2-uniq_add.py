bb9540f186543a656119c1af883899bee09090d0
Temmiebecca <temmiebecca@gmail.com>
2023-10-11 06:18:15 -0700

    0x04-python-more_data_structures

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    """Add all unique integers in a list"""
+    uniq_list = set(my_list)
+    num = 0
+
+    for i in uniq_list:
+        num += i
+
+        return (num)
